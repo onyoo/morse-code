b7d921ab7ae5e2acb8dd6e96bcc3ccdaa09ead7b
onyoo <roberto9m9@gmail.com>
2020-11-22 10:33:25 -0800

    spacing bug fixes

--- src/components/morse/index.js
+++ src/components/morse/index.js
@@ -7,7 +7,7 @@ export class Morse extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            code: [], 
+            dotTime: 0.2,
             tapStart: null, 
             tapLastEnd: null,
             key: null, 
@@ -16,85 +16,121 @@ export class Morse extends React.Component {
             morseCode: [],
             lastDuration: null,
             lastSymbol: null,
-            averageTap: 0
+            averageTap: 0,
+            reading: false
         };
       }
 
     componentDidMount = (props) => {
+        window.onblur = (event) => {
+            this.stopListening()
+        }
         document.body.addEventListener('keydown', (event) => {
             if (event.key === "Escape") {
-                this.setState({
-                    reading: !this.state.reading,
-                })
+                this.onOff()
             } else if (this.state.key !== event.key && this.state.reading) {
-                let elapsedTime = Math.floor((event.timeStamp - this.state.tapLastEnd) / 10 * this.state.dotTime)
-                let newCode = new Array(Math.floor(elapsedTime)).fill(" ")
-                // console.log(elapsedTime, newCode)
-                const newMorseCode = [...this.state.morseCode, ...newCode]
-                this.setState({
-                    key: event.key, 
-                    code: [...this.state.code, event.key], 
-                    morseCode: newMorseCode,
-                    tapLastEnd: null,
-                    tapStart: event.timeStamp
-                })
+                this.recordKeyDown(event)
             }
         });
-
         document.body.addEventListener('keyup', (event) => {
-            if (this.state.reading) {
-                const newDuration = (event.timeStamp - this.state.tapStart) / 1000
-
-                const newDurations = [...this.state.tapDurations, newDuration]
-                let avg = newDurations.reduce((a, b) => a + b) / newDurations.length
-                const newCode = 
-                    newDuration <= this.state.dotTime ? 
-                    "*" : 
-                    newDuration <= this.state.dotTime * 3.5 ?
-                    "-" : 
-                    null
-                const newMorseCode = [...this.state.morseCode, newCode]
-                this.setState({
-                    key: null, 
-                    tapStart: null,
-                    tapLastEnd: event.timeStamp,
-                    tapDurations: newDurations,
-                    lastDuration: newDuration,
-                    lastSymbol: newCode,
-                    morseCode: newMorseCode,
-                    averageTap: avg
-                })
+            if (event.key !== "Escape") {
+                this.recordKeyUp(event)
             }
         });
+    }
+
+    onOff = () => {
+        let newMorseCode = [...this.state.morseCode]
+        let command = !this.state.reading ? " Start" : " Stop"
+        if (this.state.reading) newMorseCode.push(command)
+        this.setState({reading: !this.state.reading, morseCode: newMorseCode})
+    }
+
+    stopListening = () => {
+        let newMorseCode = [...this.state.morseCode]
+        if (this.state.reading) newMorseCode.push(" Stop")
+        this.setState({
+            reading: false,
+            tapStart: null, 
+            tapLastEnd: null,
+            morseCode: newMorseCode
+        })
+    }
+
+    recordKeyDown = (event) => {
+        let newCode = []
+        if (this.state.tapLastEnd) {
+            let noKeyTime = Math.round((event.timeStamp - this.state.tapLastEnd) / 1000 * this.state.dotTime)
+            newCode = new Array(Math.floor(noKeyTime)).fill(" ")
+        }
+        let newMorseCode = [...this.state.morseCode, ...newCode]
+        this.setState({
+            key: event.key, 
+            morseCode: newMorseCode,
+            tapLastEnd: null,
+            tapStart: event.timeStamp
+        })
+    }
+
+    recordKeyUp = (event) => {
+        if (this.state.reading) {
+            const newDuration = (event.timeStamp - this.state.tapStart) / 1000
+            const tapDurations = [...this.state.tapDurations, newDuration]
+            let avg = tapDurations.reduce((a, b) => a + b) / tapDurations.length
+            const newCode = 
+                newDuration <= this.state.dotTime ? 
+                "*" : 
+                newDuration <= this.state.dotTime * 3.5 ?
+                "-" : 
+                " *Error!*"
+            const newMorseCode = [...this.state.morseCode, newCode]
+            this.setState({
+                key: null, 
+                tapStart: null,
+                tapLastEnd: event.timeStamp,
+                tapDurations: tapDurations,
+                lastDuration: newDuration,
+                lastSymbol: newCode,
+                morseCode: newMorseCode,
+                averageTap: avg
+            })
+        }
     }
 
     changeSettings = (obj) => {
         this.setState({...obj})
     }
 
-    render() {
+    reset = (event) => {
+        this.setState({
+            tapStart: null, 
+            tapLastEnd: null,
+            key: null, 
+            tapN: 0,
+            tapDurations: [],
+            morseCode: [],
+            lastDuration: null,
+            lastSymbol: null,
+            averageTap: 0,
+            reading: false
+        })
+    }
 
-        let {tapDurations, code, morseCode, ...rest} = this.state
+    render() {
+        let {tapDurations, morseCode, ...rest} = this.state
 
         let translation = morseCode.join("").split(" ").filter(str => str.length > 0).map(str => {
             return morse.morse[str]
         })
-        console.log(translation)
+        console.log(morseCode)
         return (
         <div>
-            <MorseSettings settings={this.state.settings} changeSettings={this.changeSettings}/>
-            <div id="morse-display">
-                {translation}
-            </div>
+            <MorseSettings dotTime={this.state.dotTime} changeSettings={this.changeSettings}/>
+            <div id="morse-display">{translation}</div>
                 {morseCode}
-            <h4>
-                Selected Dot Time: 
-                {this.state.level}: {this.state.dotTime}
-            </h4>
-            <h4>
-                Selected Dash Time: 
-                {this.state.level}: {this.state.dotTime * 3}
-            </h4>
+            <button onClick={this.reset}>Clear</button>
+            <h4>Selected Dot Time: {this.state.dotTime}</h4>
+            <h4>Selected Dash Time: {this.state.dotTime * 3}</h4>
             <h4>Last Duration: {this.state.lastDuration}</h4>
             <h4>Last Symbol:   {this.state.lastSymbol}</h4>
             <h4>
@@ -104,6 +140,14 @@ export class Morse extends React.Component {
             <div>
                 your state: {JSON.stringify(rest)}
                 <br/>
+                <div>
+                    Remember Remember the 5th of November:
+                    <div>
+                        {Object.keys(morse.remember).map(k => {
+                            return <div>{k}: {morse.remember[k]}</div>
+                        })}
+                    </div>
+                </div>
                 <br/>
                 Morse: {JSON.stringify(morse)}
             </div>
--- src/components/settings/index.js
+++ src/components/settings/index.js
@@ -4,28 +4,22 @@ export class MorseSettings extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            levels: [
-                {dotTime: 70/1000, label: "Easy"},
-                {dotTime: 60/1000,label:"Medium"},
-                {dotTime: 50/1000, label: "Normal"}
-            ]
-        };
+        this.state = {};
       }
-
-    componentDidMount = (props) => {
-        this.props.changeSettings(this.state.levels[0])
-    }
 
     render() {
         return (
         <div>
-            {Object.values(this.state.levels).map(level=>{
-                return <button 
-                        onClick={() => {this.props.changeSettings(level)}}>
-                        {level.label}
-                    </button>
-            })}
+            <input 
+                type="range" 
+                min="0.05" 
+                max="0.5" 
+                value={this.props.dotTime} 
+                class="slider" 
+                id="myRange" 
+                step="0.01"
+                onChange={(e) => {this.props.changeSettings({dotTime: e.target.value})}}
+            ></input>
         </div>)
     }
 }
